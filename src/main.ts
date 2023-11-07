1f961eed4eda4d0b733292076238072eea9adf16
John Dunning <fw@johndunning.com>
2023-11-06 23:37:02 -0800

    Update to latest deps with specific versions
    Sync up with the fwidgets repo.
    Add skipLibCheck to tsconfig.json so it doesn't complain about global objects.
    Add warning to ui.tsx.
    Add LICENSE.
    Remove extra scripts
    Remove patches/ and build-figma-plugin.ui.cjs.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { fwidgets } from "fwidgets/main";
 
-// all of your plugin code should be in the callback passed to fwidgets().  your
-// callback will be passed input, output and ui modules that contain functions
-// you can use to display UI controls for the plugin, one at a time.
-export default () => fwidgets(async ({ input, output, ui }) => {
+function createRectangles(
+	count: number,
+	color: RGB)
+{
+	const nodes: Array<SceneNode> = [];
+
+	for (let i = 0; i < count; i++) {
+		const rect = figma.createRectangle();
+
+		rect.x = figma.viewport.center.x + i * 150;
+		rect.fills = [{ type: "SOLID", color }];
+		figma.currentPage.appendChild(rect);
+		nodes.push(rect);
+	}
+
+	figma.currentPage.selection = nodes;
+	figma.viewport.scrollAndZoomIntoView(nodes);
+
+	return nodes;
+}
+
+// all of the code that interacts with the plugin UI should be triggered from
+// within the callback passed to fwidgets(), since the plugin window is closed
+// when the callback returns.  it will be passed input, output and ui modules
+// that contain functions you can use to display UI controls, one at a time.
+export default fwidgets(async ({ input, output, ui }) => {
 	// you can set the size of the plugin window
 	ui.setSize({
 		width: 300,
@@ -21,26 +43,25 @@ export default () => fwidgets(async ({ input, output, ui }) => {
 		integer: true
 	});
 
-	const nodes: Array<SceneNode> = [];
+	// let the user cancel before creating any rectangles
+	const button = await input.buttons("Continue?", [
+		"Create Rectangles",
+		"Cancel"
+	]);
 
-	// create the rectangles with the color and number specified by the user
-	for (let i = 0; i < count; i++) {
-		const rect = figma.createRectangle();
-
-		rect.x = figma.viewport.center.x + i * 150;
-		rect.fills = [{ type: "SOLID", color: rgb }];
-		figma.currentPage.appendChild(rect);
-		nodes.push(rect);
+	if (button === "Cancel") {
+		// returning from this function automatically closes the plugin window
+		return;
 	}
 
-	figma.currentPage.selection = nodes;
-	figma.viewport.scrollAndZoomIntoView(nodes);
+	// create the rectangles with the color and number specified by the user
+	const rects = createRectangles(count, rgb);
 
-	// collect the position and size of each rectangle
-	const rectInfo = nodes.map(({ x, y, width: w, height: h }) => ({ x, y, w, h }));
+	// collect the position and size of each rectangle node
+	const rectInfo = rects.map(({ x, y, width: w, height: h }) => ({ x, y, w, h }));
 
-	// convert that info to JSON and copy it to the clipboard
-	await output.clipboard(JSON.stringify(rectInfo, null, 2));
+	// copy the info to the clipboard as JSON
+	await output.clipboard(rectInfo);
 
 	// return a message to show in a toast after the plugin closes
 	return `${count} rectangles copied to the clipboard.`
